55611df3acdfb74be11378c9c41b7f91ed5c137b
ricardosilvacoelho <ricardo.coelho_017@hotmail.com>
2020-05-22 20:47:59 -0300

    Atividade 10

---
+++ src/main/java/br/usjt/Aula10/LoginInterceptor.java
@@ -0,0 +1,17 @@
+package br.usjt.Aula10.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		if (request.getSession().getAttribute("usuarioLogado") == null) {
+			response.sendRedirect("login");
+		}
+		return true;
+	}
+}
